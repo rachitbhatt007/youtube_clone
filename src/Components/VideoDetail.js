4ecf9c3139e3319a56a57ec940ddcc59334c9178
Rachit Bhatt <rachit.bhatt07@gmail.com>
2020-07-02 14:38:56 +0530

    done

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -7,8 +7,8 @@ const VideoDetail =({video})=>{
     }
     const videoSrc=`https://www.youtube.com/embed/${video.id.videoId}`
     return <div className="container">
-        <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={videoSrc} ></iframe>
+        <div className="embed-responsive embed-responsive-16by9 mt-3">
+        <iframe className="embed-responsive-item" src={videoSrc} allowfullscreen></iframe>
         </div>
         <div className="border my-3 p-4">
             <h6>{video.snippet.title}</h6>
--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -6,9 +6,9 @@ return(
 <div className="container my-3">
     <div className="row" onClick={()=>onVideoSelect(video)}>
     <div className="col-sm-12 col-md m-auto">
-    <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.channelTitle}/>
+    <img src={video.snippet.thumbnails.default.url} alt={video.snippet.channelTitle}/>
     </div>
-    <div className="col-sm-12 col-md-8 pull-right"><br/><br/><br/>
+    <div className="col-sm-12 col-md-8 mt-2 mr-1">
     <h6 >{video.snippet.title}</h6>
     </div>
     </div>
